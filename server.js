8aa777e928e76e1ef9714dbd56efdcd433a17f17
Hammad Tahir <tahirh@deakin.edu.au>
2018-09-05 16:28:49 +1000

    fifth commit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 const app = express();
+
+mongoose.connect('mongodb://heroku_bm1sjwq9:mtgc4ljkntakqct2stadgv6692@ds145752.mlab.com:45752/heroku_bm1sjwq9',{ useNewUrlParser: true },function(err,db){
+        if(err){
+         console.log('error');
+       }else {
+           console.log('connected to the Test db');
+       }
+     });
+
 const port = process.env.PORT || 5000;
 // API calls
 app.get('/api/hello', (req, res) => {
